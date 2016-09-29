f79f57ab82f793f95d250150d4e3fb25bd329ea5
Noisegate <marcell.marosvolgyi@gmail.com>
2016-09-29 14:15:48 +0200

    modified:   curses_scrolling.py

--- curses_scrolling.py
+++ curses_scrolling.py
@@ -27,11 +27,11 @@ class MenuDemo:
     screen = None
             
     def __init__(self):
-        self.auto = True
+        self.auto = False
         self.screen = curses.initscr()
         curses.start_color()
         curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-        self.screen.nodelay(1)
+        #self.screen.nodelay(1)
         curses.noecho()
         curses.cbreak()
         self.screen.keypad(1) 
@@ -40,6 +40,7 @@ class MenuDemo:
         self.highlightLineNum = 0
         self.markedLineNums = []
         self.getOutputLines()
+        self.updown(0)
         self.run()
 
     def run(self):
@@ -47,8 +48,9 @@ class MenuDemo:
             td = self.displayScreen()
             # get user command
             if self.auto:
-                self.updown(self.DOWN)
-                self.wait(td)
+                pass
+                #self.updown(self.DOWN)
+                #self.wait(td)
 
             c = self.screen.getch()
             if c == curses.KEY_UP: 
@@ -58,17 +60,18 @@ class MenuDemo:
             elif c == self.SPACE_KEY:
                 self.markLine()
             elif c == ord('a'):
-                self.auto != self.auto
+                self.auto = not self.auto
                 self.screen.addstr(0,0,"AUTO")
                 self.screen.refresh()
-                time.sleep(2)
+                #time.sleep(2)
             elif c == self.ESC_KEY:
                 self.exit()
 
     def wait(self, td):
-        
-        time.sleep(td/10.0)
-
+        if td==1:
+            time.sleep(5)
+        else:
+            time.sleep(td/15.0)
 
     def markLine(self):
         linenum = self.topLineNum + self.highlightLineNum
@@ -82,16 +85,23 @@ class MenuDemo:
         ### This is where you would write a function to parse lines into rows 
         ### and columns. For this demo, I'll just create a bunch of random ints
         ### !!!
-        self.outputLines = [x.strip() for x in open('lines.txt').readlines()]
+        self.outputLines = [x.strip() for x in open('mylines.txt').readlines()]
         self.nOutputLines = len(self.outputLines)
 
     def displayScreen(self):
         # clear screen
         self.screen.erase()
-
+        td=0
         # now paint the rows
         top = self.topLineNum
-        bottom = self.topLineNum+curses.LINES
+        if top<0:
+            offset = -top
+            bottom = curses.LINES-offset
+            top = 0
+        else:
+            offset = 0
+            bottom = self.topLineNum+curses.LINES
+
         for (index,line,) in enumerate(self.outputLines[top:bottom]):
             linenum = self.topLineNum + index
             if linenum in self.markedLineNums:
@@ -102,31 +112,33 @@ class MenuDemo:
             line = '%s %s' % (prefix, line,)
 
             # highlight current line            
-            if index != self.highlightLineNum:
-                self.screen.addstr(index, 0, line, curses.color_pair(1))
-            else:
-                self.screen.addstr(index, 0, line, curses.A_BOLD)
-                td = len(line)
+            #if index != self.highlightLineNum:
+            self.screen.addstr(index+offset, 0, line, curses.color_pair(1))
+
+            if (index+offset) == curses.LINES/2:
+                keepline = line
+                keepindex=index+offset
+
+                #self.screen.addstr(index+offset, 0, line, curses.A_BOLD)
+                #td = len(line)
         self.screen.refresh()
+        words = keepline.split()
+        carriage = 0
+        for i, word in enumerate(words):
+            time.sleep(.2)
+            self.screen.addstr(keepindex, carriage, word, curses.A_BOLD)
+            carriage += len(word)+1
+            self.screen.refresh()
+
+       
+
         return td
 
     # move highlight up/down one line
     def updown(self, increment):
         nextLineNum = self.highlightLineNum + increment
-
-        # paging
-        if increment == self.UP and self.highlightLineNum == 0 and self.topLineNum != 0:
-            self.topLineNum += self.UP 
-            return
-        elif increment == self.DOWN and nextLineNum == curses.LINES and (self.topLineNum+curses.LINES) != self.nOutputLines:
-            self.topLineNum += self.DOWN
-            return
-
-        # scroll highlight line
-        if increment == self.UP and (self.topLineNum != 0 or self.highlightLineNum != 0):
-            self.highlightLineNum = nextLineNum
-        elif increment == self.DOWN and (self.topLineNum+self.highlightLineNum+1) != self.nOutputLines and self.highlightLineNum != curses.LINES:
-            self.highlightLineNum = nextLineNum
+        self.topLineNum = self.highlightLineNum - curses.LINES/2 
+        self.highlightLineNum = nextLineNum        
  
     def restoreScreen(self):
         curses.initscr()
